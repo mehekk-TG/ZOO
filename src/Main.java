004787327fcb55268c664b54ff5c8b85cd20daa8
BT_3W2_27 <mehekk@nycstudents.net>
2024-12-09 13:08:53 -0500

    uygggiuny

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        Zoo zoo = new Zoo();
 
+        zoo.addAnimal(new Animal());
+        zoo.addAnimal(new Animal());
+
+        System.out.println(zoo.getAnimals());
+
+        zoo.hireEmployee(new Employee());
+        zoo.hireEmployee(new Bob());
+
+        System.out.println(zoo.getEmployees());
+
+        System.out.println(zoo);
     }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -18,6 +18,8 @@ public class Zoo {
         this.money = 1000000;
     }
 
+
+
     public void addAnimal(Animal animal) {
         animals.add(animal);
     }
@@ -26,5 +28,57 @@ public class Zoo {
         animals.remove(animal);
     }
 
-    public void
+    public void hireEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void fireEmployee(Employee employee) {
+        employees.remove(employee);
+    }
+
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public String getAnimals() {
+        String string = "Animals:\n\n";
+
+        for (int i = 0; i < animals.size(); i++) {
+           string += animals.get(i).toString() + "\n\n";
+        }
+
+        string += "\n";
+
+        return string;
+    }
+
+    public String getEmployees() {
+        String string = "Employees:\n\n";
+
+        for (int i = 0; i < employees.size(); i++) {
+            string += employees.get(i).toString() + "\n";
+        }
+
+        string += "\n";
+
+        return string;
+    }
+
+    @Override
+    public String toString() {
+        return "Open: " + open + "\nMoney: " + money + "\n" + getAnimals() + "\n" + getEmployees();
+    }
 }
